9746cd0029bf5055feda8e1263d0fdb8d0b10eea
vladislava-kova-kf <vladislava-kova-kf@users.noreply.github.com>
2026-04-29 19:25:02 +0800

    Fix macOS clipboard file-drop path normalization

--- tests/XerahS.Tests/Platform/MacOS/MacOSClipboardServiceTests.cs
+++ tests/XerahS.Tests/Platform/MacOS/MacOSClipboardServiceTests.cs
@@ -1,5 +1,7 @@
 using NUnit.Framework;
 using XerahS.Platform.MacOS;
+using System.IO;
+using System.Linq;
 
 namespace XerahS.Tests.Platform.MacOS;
 
@@ -20,5 +22,23 @@ public class MacOSClipboardServiceTests
         var files = MacOSClipboardService.ParseFileDropList("/tmp/first.png\r\n\n/tmp/second.png\r\r");
 
         Assert.That(files, Is.EqualTo(new[] { "/tmp/first.png", "/tmp/second.png" }));
+    }
+
+    [Test]
+    public void BuildPosixFileList_NormalizesRelativePathsBeforeAppleScriptClipboardSet()
+    {
+        var relativePath = Path.Combine("relative", "capture.png");
+
+        var specifier = MacOSClipboardService.BuildPosixFileList(new[] { $" {relativePath} " }).Single();
+
+        Assert.That(specifier, Is.EqualTo($"POSIX file \\\"{Path.GetFullPath(relativePath)}\\\""));
+    }
+
+    [Test]
+    public void BuildPosixFileList_SkipsInvalidOrBlankPaths()
+    {
+        var specifiers = MacOSClipboardService.BuildPosixFileList(new[] { "", "   ", "\0invalid" });
+
+        Assert.That(specifiers, Is.Empty);
     }
 }
